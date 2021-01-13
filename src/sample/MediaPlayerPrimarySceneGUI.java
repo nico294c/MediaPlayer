b2e1d8ae869d18263a010f346384fdf544b47a44
VNDPrncss <vanda.bovina@gmail.com>
2021-01-13 13:44:13 +0100

    Primary scene as of so far.

---
+++ src/sample/MediaPlayerPrimarySceneGUI.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+
+public class MediaPlayerPrimarySceneGUI extends Application {
+
+    @Override
+    public void start(Stage primarystage)
+    {
+     primarystage.setTitle("A fucking awesome Media Player");
+
+
+
+    }
+}
